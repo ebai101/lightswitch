192fe0e47890cabee3904cb8d2ef6906fa95d018
Ethan Bailey <ebailey256@gmail.com>
2023-04-08 16:22:31 -0400

    don't use ioutil

--- cmd/lights/lights.go
+++ cmd/lights/lights.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -26,7 +26,7 @@ func main() {
 			log.Fatal(err)
 		}
 
-		body, err := ioutil.ReadAll(resp.Body)
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
